04dfe1852c1b819c0022d81dd9d000b1c6e52e3b
Carlos Manente <cmanente89@gmail.com>
2024-11-21 19:55:57 -0300

    segundo commit, con implementacion de tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@
 import './App.css'
 
 function App() {
+
+  const nombre = "Luis";
+  const saludo = `Hola, ${nombre}`;
+
+  console.log(saludo);
   
 
   return (
     <div>
-      <h1>Patagoniaextrem</h1>
+      <h1 className="text-3xl font-bold underline bg-orange-500">Patagoniaextrem</h1>
     </div>
     
   )
